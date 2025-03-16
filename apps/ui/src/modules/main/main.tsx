d43834fee3acb1322f0602d45c5f595183f58b50
Wojtek Malarski <w.malarski@gmail.com>
2025-03-16 22:22:49 +0100

    something is working in plugin

--- apps/ui/src/modules/main/main.tsx
+++ apps/ui/src/modules/main/main.tsx
@@ -1,10 +1,14 @@
 import type { Component } from "solid-js";
+import { css } from "styled-system/css";
 import { Button } from "~/ui/button";
 
 export const Main: Component = () => {
   return (
     <div>
       <Button>Hello</Button>
+      <Button variant="solid">Hello</Button>
+      <Button variant="outline">Hello</Button>
+      <span class={css({ backgroundColor: "gray.dark.a4", p: 8 })}>Buu</span>
     </div>
   );
 };
