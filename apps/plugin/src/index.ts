1c80ac4a2f1bdc125e0121705fc332a4a4dfe0fa
Wojtek Malarski <w.malarski@gmail.com>
2025-03-16 23:57:16 +0100

    starts refactoring node side

--- apps/plugin/src/index.ts
+++ apps/plugin/src/index.ts
@@ -1,22 +1,49 @@
-import type { PluginMessage } from "@ftc/types";
+import type { PluginMessage, PluginMessageCreateShapes } from "@ftc/types";
 
-figma.showUI(__html__);
+figma.on("run", () => {
+  const nodes = figma.currentPage.selection;
 
-figma.ui.onmessage = (msg: PluginMessage) => {
-  if (msg.kind === "create-shapes") {
-    const numberOfRectangles = msg.count;
-
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < numberOfRectangles; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  if (nodes.length === 0) {
+    figma.notify("Error: At least 1 node has to be selected");
+    figma.closePlugin();
+    return;
   }
+
+  console.log("nodes", nodes);
+
+  // const firstNode = nodes[0]
+
+  figma.showUI(__html__);
+});
+
+figma.ui.onmessage = (message: PluginMessage) => {
+  switch (message.kind) {
+    case "create-shapes": {
+      createRectangles(message);
+      break;
+    }
+    case "cancel": {
+      figma.closePlugin();
+      break;
+    }
+    default: {
+      figma.closePlugin();
+    }
+  }
+};
+
+const createRectangles = (message: PluginMessageCreateShapes) => {
+  const numberOfRectangles = message.count;
+  const nodes: SceneNode[] = [];
+  for (let i = 0; i < numberOfRectangles; i++) {
+    const rect = figma.createRectangle();
+    rect.x = i * 150;
+    rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
+    figma.currentPage.appendChild(rect);
+    nodes.push(rect);
+  }
+  figma.currentPage.selection = nodes;
+  figma.viewport.scrollAndZoomIntoView(nodes);
 
   figma.closePlugin();
 };
